2cec8be10d43a98a876a763d0c2efe7d5f96eb49
Mostafatifa2018 <47144958+Mostafatifa2018@users.noreply.github.com>
2019-03-05 09:44:17 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,22 @@
+﻿ const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("543901526918299658")
+setInterval(function() {
+channel.send(`سانو بيتش`);
+}, 30)
+})
+
+
+client.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "تحدث") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+client.login(process.env.BOT_TOKEN); 
